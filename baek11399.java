decdc7e5169fae3a6b8ce070a3da33d57e1ebee4
kimkyury <mandarining0918@gmail.com>
2021-02-07 13:14:48 +0900

    Solved [(Silber3)No.11399]

--- baek11399.java
+++ baek11399.java
@@ -18,7 +18,6 @@ public class baek11399 {
         // time배열을 오름차순으로 정렬하자
         for (int i = 0; i < n - 1; i++) {
             boolean bigger = false;
-
             // 오름차순 되지 않은 순서가 있는지 확인하자
             for (int j = 0; j < n - 1; j++) {
                 if (time[j] > time[j + 1]) {
@@ -39,7 +38,6 @@ public class baek11399 {
 
         // answer배열을 만들어서 오름차순을 1, 2, 3, 4, 5번째까지 더하도록하고 리턴시키자
         int sum1, sum2 = 0;
-
         for (int j = 0; j < n; j++) {
             sum1 = 0;
             for (int i = 0; i <= j; i++) {
