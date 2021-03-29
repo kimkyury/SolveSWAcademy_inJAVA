b4c51c1bd45f854168a91ea098e510cd871b5220
kimkyury <mandarining0918@gmail.com>
2021-03-29 10:28:43 +0900

    I hate it

---
+++ baek10172.java
@@ -0,0 +1,9 @@
+public class baek10172 {
+    public static void main(String[] args) {
+        System.out.println("|\\_/|");
+        System.out.println("|q p|\t/}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
