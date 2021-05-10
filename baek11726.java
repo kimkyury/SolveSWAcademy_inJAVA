f96b9458d937ff599807dcf6b829531561735432
kimkyury <mandarining0918@gmail.com>
2021-05-10 15:34:17 +0900

    Runtime

--- baek11726.java
+++ baek11726.java
@@ -6,20 +6,20 @@ public class baek11726 {
     public static void main (String [] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
+        
         dp = new int [n+1];
-        int answer = cal(n);
-        // 홀수면 무조건 ro
-
-        System.out.print(answer);
-    }
-
-    static int cal(int n){
         dp[1] = 1;
         dp[2] = 2;
+        System.out.print(cal(n));
+    }
 
+    static public int cal(int n){
+        if(dp[n] >0){
+            return dp[n];
+        }
         //2xn을 만들려면, 2x(n-1)에서 [ | ] 를 하나씩 추가하거나
         //  2X(n-2)에서 [ = ] 를 하나씩 추가한다
-        dp[n] = dp[n-1] + dp[n-2];
+        dp[n] = (cal(n-1) + cal(n-2)) % 10007;
         return dp[n];
     }
 }
