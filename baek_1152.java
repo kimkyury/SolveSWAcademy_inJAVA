c1a459e4fd2cce475f2b90394045fbe54fee98e7
kimkyury <mandarining0918@gmail.com>
2021-11-04 16:17:08 +0900

    solve 1152

---
+++ baek_1152.java
@@ -0,0 +1,16 @@
+import util.*;
+import io.*;
+
+public class baek_1152 {
+    public static void main (String [] args){
+        // int answer =0 ; // 개수 카운트
+		Scanner sc = new Scanner(System.in);
+		String words = in.nextLine();
+		sc.close();
+ 
+		StringTokenizer st = new StringTokenizer(sc," ");
+		
+
+		System.out.println(st.countTokens());	
+    }
+}
