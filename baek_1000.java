7ca2d4ee2e64ec4eb34537d4cdba3f84d19ac638
kimkyury <mandarining0918@gmail.com>
2021-11-05 23:16:55 +0900

    solved No.1000

---
+++ baek_1000.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class baek_1000 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+
+        int c = a + b;
+
+        System.out.print(c);
+    }
+}
--- baek_1152.java
+++ baek_1152.java
@@ -1,16 +1,16 @@
-import util.*;
-import io.*;
+
+import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class baek_1152 {
-    public static void main (String [] args){
-        // int answer =0 ; // 개수 카운트
+	public static void main(String[] args) {
+		// int answer =0 ; // 개수 카운트
 		Scanner sc = new Scanner(System.in);
-		String words = in.nextLine();
+		String words = sc.nextLine();
 		sc.close();
- 
-		StringTokenizer st = new StringTokenizer(sc," ");
-		
 
-		System.out.println(st.countTokens());	
-    }
+		StringTokenizer st = new StringTokenizer(words, " ");
+
+		System.out.println(st.countTokens());
+	}
 }
