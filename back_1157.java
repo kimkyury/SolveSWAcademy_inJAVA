703c48e0901894f937c2b47760b53c240ddb45cd
kimkyury <mandarining0918@gmail.com>
2021-11-08 23:47:44 +0900

    예외발생, abccc

--- back_1157.java
+++ back_1157.java
@@ -1,53 +1,50 @@
-import java.io.*;
+
+//import java.io.*;
 import java.util.*;
-import java.util.ArrayList;
 
 public class back_1157 {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner sc = new Scanner(System.in);
+        // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // String str = br.readLine();
+        String str = sc.nextLine();
+        sc.close();
 
-        String str = br.readLine();
         int len = str.length();
-
         boolean[] isPassed = new boolean[len];
         int[] arr = new int[len];
-
         String answer;
-        // ArrayList<Integer> intArr = new ArrayList
-
-        for (int i = 0; i < len; i++) {
-            arr[i] = 0;
-        }
 
         int cnt;
         for (int i = 0; i < len; i++) {
             cnt = 0;
-            char apb = str.charAt(i);
+            if (isPassed[i] == true)
+                continue;
             for (int j = 0; j < len; j++) {
-                if (apb == str.charAt(j) && isPassed[j] == false) {
-                    isPassed[j] = true;
+                if (str.charAt(i) == str.charAt(j)) {
                     cnt++;
+                    isPassed[j] = true;
                 }
             }
             arr[i] = cnt;
         }
 
         int max = arr[0];
-        for (int i = 1; i < len; i++) {
+        for (int i = 0; i < len; i++) {
             if (max < arr[i]) {
                 max = arr[i];
             }
         }
 
-        int maxCnt = 0;
-        for (int i = 0; i < len; i++) {
+        boolean isSame = false;
+        for (int i = 1; i < len; i++) {
             if (max == arr[i]) {
-                maxCnt++;
+                isSame = true;
             }
         }
 
-        if (maxCnt > 1) {
+        if (isSame == true) {
             answer = String.valueOf('?');
         } else {
             answer = String.valueOf(max);
