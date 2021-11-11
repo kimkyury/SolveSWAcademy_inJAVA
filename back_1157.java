b93539f95743abfdf7a093ec50e6db80cf161bae
kimkyury <mandarining0918@gmail.com>
2021-11-11 15:13:23 +0900

    Chore: 틀렸대

--- back_1157.java
+++ back_1157.java
@@ -1,55 +1,60 @@
 
-//import java.io.*;
+import java.io.*;
 import java.util.*;
 
 public class back_1157 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        Scanner sc = new Scanner(System.in);
-        // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        // String str = br.readLine();
-        String str = sc.nextLine();
-        sc.close();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
 
         int len = str.length();
         boolean[] isPassed = new boolean[len];
-        int[] arr = new int[len];
-        String answer;
+        int[] arrCnt = new int[len]; // 각 단어의 개수
+        char answer = ' '; // 리턴 값
 
+        // 문자열을 소문자로 바꾸자
+        str = str.toLowerCase();
+
+        // 입력된 각 문자의 개수를 세어 보자, Mississipi라고 생각해보자
         int cnt;
         for (int i = 0; i < len; i++) {
+
             cnt = 0;
-            if (isPassed[i] == true)
+            if (isPassed[i] == true) {
                 continue;
+            }
             for (int j = 0; j < len; j++) {
                 if (str.charAt(i) == str.charAt(j)) {
                     cnt++;
                     isPassed[j] = true;
                 }
             }
-            arr[i] = cnt;
+            arrCnt[i] = cnt;
         }
 
-        int max = arr[0];
+        // 각 문자열의 개수가 arrCnt에 담겼으니, 누가 가장 큰 지 알아내자
+        int max = arrCnt[0];
+        answer = str.charAt(0);
         for (int i = 0; i < len; i++) {
-            if (max < arr[i]) {
-                max = arr[i];
+            if (max < arrCnt[i]) {
+                max = arrCnt[i];
+                answer = str.charAt(i); // 최대값을 찾을 때마다 str로 바꿔주자
             }
         }
 
-        boolean isSame = false;
-        for (int i = 1; i < len; i++) {
-            if (max == arr[i]) {
-                isSame = true;
+        // 최대 출현 빈도수를 가지는 문자가 두 개 이상 나타나면 answer을 '?'로 바꿔주자
+        int n = 0;
+        for (int i = 0; i < len; i++) {
+            if (max == arrCnt[i]) {
+                n++;
             }
         }
 
-        if (isSame == true) {
-            answer = String.valueOf('?');
-        } else {
-            answer = String.valueOf(max);
+        if (n > 1) {
+            answer = '?';
         }
 
-        System.out.println(answer);
+        System.out.print(String.valueOf(answer));
     }
 }
