869d0c5e09f013429cd1a94afa1fe27ba64949c9
kimkyury <mandarining0918@gmail.com>
2021-11-14 12:26:56 +0900

    Solved No.11654

---
+++ baek_11654.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+public class baek_11654 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char ch = br.readLine().charAt(0);
+
+        int ascii = ch;
+
+        System.out.print(ascii);
+
+    }
+}
