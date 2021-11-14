8fd479277caa2422d611aa979c8d4b379c42bb09
kimkyury <mandarining0918@gmail.com>
2021-11-14 11:10:41 +0900

    Solved No.2675

--- baek_1157.java
+++ baek_1157.java
@@ -1,5 +1,4 @@
 import java.io.*;
-import java.util.*;
 
 public class baek_1157 {
     public static void main(String[] args) throws IOException {
---
+++ baek_2675.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class baek_2675 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            String answer = "";
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int times = Integer.parseInt(st.nextToken());
+            String word = st.nextToken();
+
+            for (int j = 0; j < word.length(); j++) {
+                for (int k = 0; k < times; k++) {
+                    System.out.print(word.charAt(j));
+                }
+            }
+            System.out.println();
+        }
+    }
+}
