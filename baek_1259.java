4b1356c64795fb6b429d0f9eeb5edfa679e4a72c
kimkyury <mandarining0918@gmail.com>
2021-11-17 00:11:27 +0900

    Solved No.1259

--- baek_1259.java
+++ baek_1259.java
@@ -2,30 +2,34 @@ import java.io.*;
 import java.util.*;
 
 public class baek_1259 {
-    public class void main(String [] args) throws IOException{
-        BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         ArrayList<String> strings = new ArrayList<String>();
         String str;
-        while(true){
+
+        while (true) {
             str = br.readLine();
-            if(str.equals('0')){
-                return;
-            }else{
-                strings.add(br.readLine());
+            if (str.equals(String.valueOf('0'))) {
+                break;
+            } else {
+                strings.add(str);
             }
         }
 
-
         String str2;
-        for(int i =0; i< strings.size(); i++){
+        for (int i = 0; i < strings.size(); i++) {
             str2 = strings.get(i);
             str = "";
-            for(int j=0; j< strings.get(i).length(); j++){
-                str +=  
+            for (int j = strings.get(i).length() - 1; j >= 0; j--) {
+                str += strings.get(i).charAt(j);
             }
-        })
-
+            if (str2.equals(str)) {
+                System.out.println("yes");
+            } else {
+                System.out.println("no");
+            }
+        }
 
     }
 }
