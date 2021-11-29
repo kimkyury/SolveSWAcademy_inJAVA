c41ff7f3baa7990178ab49742815de3ae60588a8
kimkyury <mandarining0918@gmail.com>
2021-11-29 23:20:27 +0900

    solved No.2292

---
+++ baek_2292.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class baek_2292 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        // 수열 1, 6, 12, 18, 24
+        // a1=1,
+        // a2부터는 +6임.
+        //
+        int answer;
+        int number = 2;
+
+        if (n == 1) {
+            answer = 1;
+        } else { // n=1이상일 때,
+            answer = 1;
+            while (number <= n) {
+                number += (6 * answer);
+                answer++;
+            }
+        }
+        System.out.print(answer);
+    }
+}
